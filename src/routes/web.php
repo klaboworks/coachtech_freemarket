faa6a1601466e2f51a1dc24aed06c44b58c99b3f
KenTanaka <klaboworks@gmail.com>
2024-12-29 14:46:46 +0900

    ログイン機能実装+ヘッダー表示設定変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
     //todo: メール認証設定後、'verified'追記
-    Route::get('/', [ItemController::class, 'index']);
 });
+
+Route::get('/', [ItemController::class, 'index']);
