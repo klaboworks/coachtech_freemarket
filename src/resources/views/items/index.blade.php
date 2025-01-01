ae8b9b77451e17012c0ac4dedaf98ad120fc11d0
KenTanaka <klaboworks@gmail.com>
2025-01-01 17:36:55 +0900

    商品一覧画面ビュー作成

--- src/resources/views/items/index.blade.php
+++ src/resources/views/items/index.blade.php
@@ -8,14 +8,31 @@
 
 @section('content')
 <section class="index">
-    <div class="section__inner">
-        商品一覧画面
-
-        @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
+    <div class="index__inner">
+        <div class="index__heading">
+            <div class="heading-menu">
+                <a href="">おすすめ</a>
+                <a href="">マイリスト</a>
+            </div>
         </div>
-        @endif
+        <div class="index__body">
+            <div class="items-panel">
+                @foreach($items as $item)
+                <div class="item-unit">
+                    <a href="" class="item-image">
+                        <img src="{{$item->item_image}}" alt="">
+                    </a>
+                    <h2 class="item-name">{{$item->item_name}}</h2>
+                </div>
+                @endforeach
+
+                @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+                @endif
+            </div>
+        </div>
     </div>
 </section>
 @endsection
