0d640405a35d7ad833c0d12836e1c9edb35bbc8b
KenTanaka <klaboworks@gmail.com>
2025-01-02 15:39:09 +0900

    商品検索機能実装

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Item;
 
 class ItemController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $items = Item::all();
+        $items = Item::exceptCurrentUser()->Search($request->serach)->get();
         return view('items.index', compact('items'));
     }
 }
--- src/app/Models/Item.php
+++ src/app/Models/Item.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Item extends Model
 {
-    //
+    public function scopeExceptCurrentUser($query)
+    {
+        if (Auth::check()) {
+            return $query->where('user_id', '!=', Auth::user()->id);
+        }
+
+        return $query;
+    }
+
+    public function scopeSearch($query, $search)
+    {
+        if ($search) {
+            $query->where('item_name', 'like', '%' . $search . '%');
+        }
+    }
 }
