fc60169751d9313da81536c2a194175116711f2a
KenTanaka <klaboworks@gmail.com>
2025-01-02 16:17:58 +0900

    自分のアイテムを除外して表示＆商品検索機能実装

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -7,9 +7,9 @@ use App\Models\Item;
 
 class ItemController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $items = Item::all();
+        $items = Item::exceptCurrentUser()->Search($request->serach)->get();
         return view('items.index', compact('items'));
     }
 }
