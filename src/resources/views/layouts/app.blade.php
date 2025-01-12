7d322d65c55e0393bcd034f2695d49724082a4b8
KenTanaka <klaboworks@gmail.com>
2025-01-12 16:32:44 +0900

    ヘッダーレスポンシブ対応

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,77 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     @yield('css')
+    <script src="{{ asset('js/header.js') }}" defer></script>
     @yield('script')
     <title>coachtehフリマ | @yield('title')</title>
-    <style>
-        .header {
-            height: 48px;
-            background-color: black;
-        }
-
-        .header__inner {
-            display: flex;
-            justify-content: space-between;
-            height: 100%;
-            width: calc(100% - 24px);
-            align-items: center;
-        }
-
-        .logo {
-            margin-left: 24px;
-            flex: 1;
-        }
-
-        .search-bar {
-            flex: 1;
-
-            >input {
-                width: 100%;
-                padding-left: 24px;
-            }
-        }
-
-        .nav-menu {
-            display: flex;
-            gap: 20px;
-            flex: 1;
-            justify-content: flex-end;
-            align-items: center;
-
-            .login,
-            .logout,
-            .mypage {
-                color: white;
-                border: none;
-                background: none;
-                text-decoration: none;
-            }
-
-            .list-items {
-                background-color: white;
-                padding: 2px 16px 0;
-                color: black;
-                text-decoration: none;
-                border-radius: 2px;
-            }
-        }
-    </style>
 </head>
 
 <body>
     <header class="header">
-        <div class="header__inner">
+        <div class="header__inner flex-row">
             <div class="logo">
                 <a href="{{route('items.index')}}">
-                    <img src="{{ asset('icon/logo.svg') }}" alt="">
+                    <img src="{{ asset('images/icons/logo.svg') }}" alt="">
                 </a>
             </div>
+
             @if (!Route::is('register', 'login'))
-            <form action="{{route('items.index')}}" method="get" class="search-bar">
-                <input name="search" type="text" placeholder="なにをお探しですか？" value="{{old('search',session('search_query'))}}">
-            </form>
+            <div>
+                <form action="{{route('items.index')}}" method="get" class="search-bar">
+                    <input name="search" type="text" placeholder="なにをお探しですか？" value="{{old('search',session('search_query'))}}">
+                </form>
+            </div>
+
             <div class="nav-menu">
                 @if (!Auth::check())
                 <a href="{{ route('login') }}" class="login">ログイン</a>
@@ -90,7 +43,37 @@
                 <a href="/mypage" class="mypage">マイページ</a>
                 <a href="{{route('sell.index')}}" class="list-items">出品</a>
             </div>
+
+            <div class=humburger__open>
+                <img src="{{ asset('images/icons/menu.png') }}" alt="">
+            </div>
             @endif
+
+            <div class="humburger-menu__elements text-center flex-column">
+                <div class="humburger__close">
+                    <img src="{{asset('images/icons/close.png')}}" alt="">
+                </div>
+                <form action="{{route('items.index')}}" method="get" class="humburger__search-bar">
+                    <input name="search" type="text" placeholder="なにをお探しですか？" value="{{old('search',session('search_query'))}}">
+                </form>
+                @if (!Auth::check())
+                <div>
+                    <a href="{{ route('login') }}" class="humburger__login no-decoration">ログイン</a>
+                </div>
+                @endif
+                @if (Auth::check())
+                <form action="/logout" method="post">
+                    @csrf
+                    <button class="humburger__logout">ログアウト</button>
+                </form>
+                @endif
+                <div>
+                    <a href="/mypage" class="humburger__mypage no-decoration">マイページ</a>
+                </div>
+                <div>
+                    <a href="{{route('sell.index')}}" class="humburger__list-items no-decoration">出品</a>
+                </div>
+            </div>
         </div>
     </header>
     @yield('content')
